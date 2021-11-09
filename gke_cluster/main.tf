184ce0041fff924ee6accf0b1bb22e8b5f3174fe
Mykhailo Gerasymenko <phelpsmisha@gmail.com>
2021-11-09 16:24:40 +0200

    test commit i2

--- gke_cluster/main.tf
+++ gke_cluster/main.tf
@@ -11,8 +11,8 @@ resource "google_container_cluster" "primary" {
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
-  #remove_default_node_pool = true
-  #initial_node_count       = 1
+  remove_default_node_pool = true
+  initial_node_count       = 1
 }
 resource "google_container_node_pool" "test_node_pool" {
   #for_each = local.list_node_pool
